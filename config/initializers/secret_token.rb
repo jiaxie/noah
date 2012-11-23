a34b6b1b60013cb4bd21d1bda7f6ed8445a5acc4
Xie Jia <jiaxie@thoughtworks.com>
2012-11-23 15:17:25 +0800

    NOA-1: create a new app. Jia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noah::Application.config.secret_token = '73a014481f46ba00a3ec32a02c871acd13ff2cc145fc8e7c501c78db03968d527d3175dc3c1c5257395ee4443b57ba9ba642b6962a096b42210e6d05d15f262b'
