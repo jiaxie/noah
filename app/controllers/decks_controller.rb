b65996b39af0aef2c195c02da74d0b2f86113621
baimingming <mmbai@thouthtworks.com>
2012-12-13 14:28:09 +0800

    Fixed feature pick bug and remove some rubbish. Bmy

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -8,16 +8,6 @@ class DecksController < ApplicationController
 
   end
 
-  def delete_blog
-    @current_deck = Deck.find(session[:id])
-    @features = @current_deck.features
-    @current_feature = @features.find(session[:feature_id])
-    @blog = @current_feature.blogs.find(params[:bid])
-    @blog.destroy
-    @blogs = @current_feature.blogs
-    render 'show'
-  end
-
   def show_default
     @current_deck = Deck.find(params[:id])
     @features = @current_deck.features
@@ -25,14 +15,5 @@ class DecksController < ApplicationController
     @blogs = @current_feature.blogs
     session[:deck_id] = params[:id]
     session[:feature_id] = @current_feature.id
-  end
-
-  def choose_feature
-    @current_deck = Deck.find(params[:id])
-    @features = @current_deck.features
-    @current_feature = @features.find(params[:fid])
-    @blogs = @current_feature.blogs
-    session[:feature_id] = params[:fid]
-    render 'show'
   end
 end
--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,8 +1,8 @@
 class FeaturesController < ApplicationController
   def show
-    @current_deck = Deck.find(session[:deck_id])
+    @current_deck = Deck.find(params[:deck_id])
     @features = @current_deck.features
-    @current_feature = @features.find :first
+    @current_feature = @features.find(params[:id])
     @blogs = @current_feature.blogs
   end
 end
