1b360529edc8c49a934881a320c559f20b9a4be3
baimingming <mmbai@thouthtworks.com>
2012-12-14 10:47:11 +0800

    Add cancel button. Bmy

--- app/assets/javascripts/edit_form_render.js
+++ app/assets/javascripts/edit_form_render.js
@@ -1,9 +1,17 @@
 $(function(){
-  // $(".edit").click(render_edit_form);
+  hide_cancel_button();
   $('.edit').click(render_edit_form);
+  $('.cancel').click(cancel_edit);
 });
 
+var hide_cancel_button = function(){
+  $('.cancel').each(function(){
+    $(this).hide();
+  })
+}
+
 var render_edit_form = function(){
+  $(this).hide();
   var bid = this.id;
 
   $('.blog-title').each(function(){
@@ -18,6 +26,12 @@ var render_edit_form = function(){
     }
   })
 
+  $('.cancel').each(function(){
+    if(this.id == bid){
+      $(this).show();
+    }
+  })
+
   $.ajax("/blogs/get_blog/" + bid, {dataType:'html'}).success(function(response){
     $('.edit-form').each(function(){
       if(this.id == bid){
@@ -27,4 +41,23 @@ var render_edit_form = function(){
     })
   });
   return false;
+}
+
+var cancel_edit = function(){
+  $('form').hide();
+
+  var bid = this.id;
+  $('.blog-title').each(function(){
+      if(this.id == bid){
+        $(this).show();
+      }
+    })
+
+  $('.blog-content').each(function(){
+    if(this.id == bid){
+      $(this).show();
+    }
+  })
+
+  $(this).hide();
 }
