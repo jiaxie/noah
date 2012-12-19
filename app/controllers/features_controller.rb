fcd13d13a041ad209a242834f85581319d60c1a3
baimingming <mmbai@thouthtworks.com>
2012-12-19 16:12:15 +0800

    Sorted blogs be created time DESC. Bmy

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -3,7 +3,7 @@ class FeaturesController < ApplicationController
     @current_deck = Deck.find(params[:deck_id])
     @features = @current_deck.features
     @current_feature = @features.find(params[:id])
-    @blogs = @current_feature.blogs
+    @blogs = @current_feature.blogs.order('created_at DESC')
     session[:deck_id] = params[:deck_id]
     session[:feature_id] = @current_feature.id
   end
