70c04f96cc2ee8be4fee811d7d6b10426e1cfdf6
baimingming <mmbai@thouthtworks.com>
2012-12-20 12:57:49 +0800

    Adjust header style and using fade-in display. Bmy

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,16 +1,58 @@
 $(function(){
   hide_and_bundle();
+  scroll_magic();
+});
+
+$(window).scroll(function(){ 
+  markElephant();
 });
 
 var hide_and_bundle = function(){
   $(".time-line").hide();
 
-  $(".mouse-detector").hover(function(){
-    $(".time-line").show();
+  // $(".mouse-detector").hover(function(){
+  //   $(".time-line").show();
+  // })
+
+  // $(".time-line").bind('mouseout',function(){
+  //   $(".time-line").hide();
+  // })
+}
+
+var scroll_magic = function(){
+  $(".blog-item").each(function(){
+    $(this).addClass("rat");
   })
 
-  $(".time-line").bind('mouseout',function(){
-    $(".time-line").hide();
+  $(".blog-item :first").removeClass("rat");
+  $(".blog-item :first").addClass("elephant");
+}
+
+var markElephant = function(){
+  var minDistance = document.documentElement.scrollHeight;
+  var nearest = null;
+
+  $(".blog-item").each(function(){
+    var dis = this.getBoundingClientRect().top;
+    if(dis >= 0 && dis < minDistance){
+       minDistance = dis;
+       nearest = this;
+    }
   })
+
+  if(nearest !== null){
+    var id = nearest.id;
+
+    $(".blog-item").each(function(){
+      if(this.id == id){
+         $(this).removeClass("rat");
+         $(this).addClass("elephant");
+      }
+      else{
+        $(this).addClass("rat");
+      }
+    })
+
+  }
 }
 
